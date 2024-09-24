64f1278c4c7d1c6dcab066f5638badecdbce44de
Viktor <komvik.slack@gmail.com>
2024-09-24 11:26:54 +0200

    Start WetherApp

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: WeatherApp(),
+    );
+  }
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+        child: Text("Wether App", style: TextStyle(fontSize: 40)));
+  }
+}
